0b839902b04adb41d0151bb7805c52d2d945cabf
safoan.zk <95578112+pegasus4me@users.noreply.github.com>
2023-09-09 22:01:09 +0200

    Update intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
   return (
     <div className=" p-3  ">
       <p className="font-light text-[#888888] text-lg">
-        <span className="font-normal text-white">Hi there</span>, I'm safoan - I'm 22 y/o web3 front end <span className="text-white">de</span>veloper, <span className="text-white">I like
+        <span className="font-normal text-white">Hi there</span>, I'm safoan - I'm 22 y/o web3/fullstack <span className="text-white">de</span>veloper, <span className="text-white">I like
         bu</span>ilding <span className="text-white">new things and innovate</span>, I enjoy <span className="text-white">blockchain</span> and <span className="text-white">design</span>.
       </p>
       <p className="font-light text-[#888888] text-lg mt-3 mb-3">Right now I'm building an decentralised <span className="text-white font-normal">crowdfounding</span> app.</p>

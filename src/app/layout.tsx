3365abaa16402cfe030d613037a4a7d78c1b5b8a
soso <95578112+pegasus4me@users.noreply.github.com>
2023-10-12 10:55:03 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-[#161617]">
     <body className={`${inter.className} max-w-[820px] m-auto`}>
       {children}
       <Footer/>

7fd40cafc5e0ad38626ef3200bfd5a1bbe04c6f8
soso <95578112+pegasus4me@users.noreply.github.com>
2023-12-03 15:07:59 +0100

    Update intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
         <span className="font-normal text-white">Hi there</span>, I'm safoan - I'm 22 y/o web3/fullstack <span className="text-white">de</span>veloper, <span className="text-white">I like
         bu</span>ilding <span className="text-white">new things and innovate</span>, I enjoy <span className="text-white">blockchain</span> and <span className="text-white">design</span>.
       </p>
-      <p className="font-light text-[#888888] text-lg mt-3 mb-3">Right now I'm building an decentralised <span className="text-white font-normal">crowdfounding</span> app.</p>
+      <p className="font-light text-[#888888] text-lg mt-3 mb-3">Right now I'm building a webinar platform <span className="text-white font-normal">designed for Products demos </span> for B2B companies.</p>
       <div className="min-h-[70px] border-l border-r border-dotted opacity-40">
       </div>
     </div>
